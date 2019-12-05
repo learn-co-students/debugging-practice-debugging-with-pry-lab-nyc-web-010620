b5c1b5a00842c7d8507cf41187469ddf168b6214
Miles Higbie <miles.higbie@gmail.com>
2019-12-05 01:06:59 +0000

    Done.

--- fix_using_tests/the_final_frontier.rb
+++ fix_using_tests/the_final_frontier.rb
@@ -1,18 +1,21 @@
+lrequire "pry"
+
 def generate_star_date
   (rand(100000) + 400000) / 10.0
 end
 
 
 def state_log(star_date)
-  "Captain's Log, star date #{star_date}."
+ puts "Captain's Log, star date #{star_date}."
 end
 
 def crew
-  ["Geordi", "Data", "Worf", "William", "Beverly", "Deanna"]
-end
+["Geordi", "Data", "Worf", "William", "Beverly", "Deanna"]
+end 
 
-def greet_crew (crew)
-  crew.each {|crew_member| "Hello #{crew_member}."}
+
+def greet_crew(array)
+  array.each {|crew_member| puts "Hello #{crew_member}."}
 end
 
 def engage
